a09c439f0384379fa403f079087b28551f73dc9b
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-13 00:27:17 +0530

    add variable : myQueue(int[]), beginning(int), top(int), isQueueCreated(boolean)

--- src/queueUsingCircularArray/QueueUsingCircularArray.java
+++ src/queueUsingCircularArray/QueueUsingCircularArray.java
@@ -1,4 +1,8 @@
 package queueUsingCircularArray;
 
 public class QueueUsingCircularArray {
+    private int[] myQueue;
+    private int beginning;
+    private int top;
+    private boolean isQueueCreated;
 }
