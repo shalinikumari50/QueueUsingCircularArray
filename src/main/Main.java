92ca424db8b24e372f31a2b9191ee47833d07871
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-13 00:48:59 +0530

    add object : queue of class : QueueUsingCircularArray

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import queueUsingCircularArray.QueueUsingCircularArray;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        QueueUsingCircularArray queue = new QueueUsingCircularArray();
     }
 
 }
