17d4e4ce633f1223bf61bee9c41009376d460650
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-13 00:35:16 +0530

    add method createQueue(void) to create a queue

--- src/queueUsingCircularArray/QueueUsingCircularArray.java
+++ src/queueUsingCircularArray/QueueUsingCircularArray.java
@@ -1,8 +1,30 @@
 package queueUsingCircularArray;
 
+import java.util.Scanner;
+
 public class QueueUsingCircularArray {
     private int[] myQueue;
     private int beginning;
     private int top;
     private boolean isQueueCreated;
+
+    public void createQueue() throws NumberFormatException {
+        if (isQueueCreated) {
+            System.out.println("Queue already created. Delete queue to create new");
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size");
+        int size = Integer.parseInt(scanner.nextLine().trim());
+        if (size <= 0) {
+            System.out.println("Size should be greater tha zero");
+            return;
+        }
+        myQueue = new int[size];
+        beginning = -1;
+        top = -1;
+        isQueueCreated = true;
+        System.out.println("Queue of size " + size + " created.");
+
+    }
 }
