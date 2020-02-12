ff3f4ef7855ffae5db83b66470caa949ec1fe1df
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-13 00:49:56 +0530

    add variable : choice(int)

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         QueueUsingCircularArray queue = new QueueUsingCircularArray();
+        int choice = -1;
+
     }
 
 }
