43e2b688e58f4b71efedfe7c6dde5f1935f6db20
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-13 00:54:27 +0530

    add while loop to implement menu-driven program

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,52 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         QueueUsingCircularArray queue = new QueueUsingCircularArray();
         int choice = -1;
+        queue.printMenu();
+        while (choice != 0) {
+            System.out.println("Enter choice");
+            try {
+                choice = Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid input");
+                continue;
+            }
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    try {
+                        queue.createQueue();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 2:
+                    try {
+                        queue.enqueue();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 3:
+                    queue.dequeue();
+                    break;
+                case 4:
+                    queue.peekInQueue();
+                    break;
+                case 5:
+                    queue.display();
+                    break;
+                case 6:
+                    queue.deleteQueue();
+                    break;
+                case 7:
+                    queue.printMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }
 
     }
 
